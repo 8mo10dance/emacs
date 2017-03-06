366d905ca99e35865e69abd886986c00bd7b7145
sunda <sunda@stu.kobe-u.ac.jp>
2017-03-07 01:43:08 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,26 @@
+;; load-path
+(defun add-to-load-path (&rest paths) ;; &restは引数のリストをまとめて関数に渡す
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+	      (expand-file-name (concat user-emacs-directory path))))
+	(add-to-list 'load-path default-directory)
+	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+	    (normal-top-level-add-subdirs-to-load-path))))))
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Cask
+(require 'cask) ;; for only mac
+(cask-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; prettier
+(require 'prettier-js)
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
