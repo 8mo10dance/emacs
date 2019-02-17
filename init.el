d44101a874ea277db5fefc56965b9c1d6ee1aa8b
dexmon <dexmon@example.com>
2019-02-17 21:20:24 +0900

    feat: 不要なコードを削除する

--- init.el
+++ init.el
@@ -9,16 +9,6 @@
 
 (init-loader-load)
 
-;; load-path
-(defun add-to-load-path (&rest paths) ;; &restは引数のリストをまとめて関数に渡す
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-	      (expand-file-name (concat user-emacs-directory path))))
-	(add-to-list 'load-path default-directory)
-	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-	    (normal-top-level-add-subdirs-to-load-path))))))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; other window
