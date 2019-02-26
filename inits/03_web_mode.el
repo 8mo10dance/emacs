2984ecafdbe4bf47f85cc656100269b5ec1f3e65
sunda <sunda@synchro-food.co.jp>
2019-02-26 13:16:51 +0900

    feat: 正規表現の書き方を変える

--- inits/03_web_mode.el
+++ inits/03_web_mode.el
@@ -1,20 +1,20 @@
 (setq auto-mode-alist
-      (append '(("\\.jsx?$" . web-mode))
+      (append '(("\\.jsx?\\'" . web-mode))
 	      auto-mode-alist))
 (setq auto-mode-alist
-      (append '(("\\.tsx?$" . web-mode))
+      (append '(("\\.tsx?\\'" . web-mode))
 	      auto-mode-alist))
 (setq auto-mode-alist
-      (append '(("\\.jsp$'" . web-mode))
+      (append '(("\\.jsp\\'" . web-mode))
 	      auto-mode-alist))
 (setq auto-mode-alist
-      (append '(("\\.erb$'" . web-mode))
+      (append '(("\\.erb\\'" . web-mode))
 	      auto-mode-alist))
 (setq auto-mode-alist
-      (append '(("\\.scss$" . web-mode))
+      (append '(("\\.scss\\'" . web-mode))
 	      auto-mode-alist))
 
-;; Web-mode is a popular mode for editing .js and .jsx files, but it is used to edit other template files too. If you want to hook prettier-js to web-mode for .js and .jsx files only, you can define a helper function like this: 
+;; Web-mode is a popular mode for editing .js and .jsx files, but it is used to edit other template files too. If you want to hook prettier-js to web-mode for .js and .jsx files only, you can define a helper function like this:
 (defun enable-minor-mode (my-pair)
   "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
   (if (buffer-file-name)
