94e75517c1cf48cad36ad942d062f3d816b68cdd
sunda <sunda@synchro-food.co.jp>
2019-02-26 13:18:33 +0900

    feat .rbと.schemaをruby-modeで開くようにする

--- inits/10_rails.el
+++ inits/10_rails.el
@@ -1,3 +1,10 @@
+(setq auto-mode-alist
+      (append '(("\\.rb\\'" . ruby-mode))
+	      auto-mode-alist))
+(setq auto-mode-alist
+      (append '(("\\.schema\\'" . ruby-mode))
+	      auto-mode-alist))
+
 ;; disable magic comments such as "coding: utf-8"
 (setq ruby-insert-encoding-magic-comment nil)
 
