a28d6613994fdfcad4d024e24b6d4797afaf84b1
sunda <8mo10dance@protonmail.com>
2020-11-22 11:42:49 +0900

    theme を変える

--- inits/04_themes.el
+++ inits/04_themes.el
@@ -3,6 +3,6 @@
   (doom-themes-enable-italic t)
   (doom-themes-enable-bold t)
   :config
-  (load-theme 'doom-city-lights t)
+  (load-theme 'doom-one-light t)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
