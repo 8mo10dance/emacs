5637f42626e62c24ab3cd90479b6ce70079c8a23
sunda <8mo10dance@protonmail.com>
2021-02-28 19:00:04 +0900

    theme を変更する

--- inits/04_themes.el
+++ inits/04_themes.el
@@ -3,6 +3,6 @@
   (doom-themes-enable-italic t)
   (doom-themes-enable-bold t)
   :config
-  (load-theme 'doom-one-light t)
+  (load-theme 'doom-one t)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
